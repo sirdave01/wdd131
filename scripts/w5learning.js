fcdd41034e1d839c84d9f92836dd60db5865f8d9
sirdave01 <d08178084956@gmail.com>
2025-09-27 00:10:40 -0700

    okay

--- scripts/w5learning.js
+++ scripts/w5learning.js
@@ -1,3 +1,9 @@
+// The fetchData function simulates fetching data from a server by using the setTimeout function to create a 2-second delay.
+// After the delay, it calls the provided callback function, passing the string
+// 'Data has been fetched' as an argument. In this context,
+// the processData function is passed as the callback to fetchData,
+// and it logs the received data to the console when called.
+
 // A common use of a callback functions in JavaScript is for asynchronous operations,
 // such as handling events or making asyncchronous requests.
 // Here is a simulated example:
@@ -17,3 +23,4 @@ function processData(data) {
 
 // Call the fetchData function and pass the processData function as an argument.
 fetchData(processData);
+
